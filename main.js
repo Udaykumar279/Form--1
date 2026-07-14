ed34bb53dcc5c3aa25e2128078635048db8c5b6e
Udaykumar <udaykumarc3126@gmail.com>
2026-07-14 16:07:38 +0100

    mailer code changed

--- main.js
+++ main.js
@@ -32,6 +32,9 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  requireTLS: true,
+  logger: true,
+  debug: true,
    auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
